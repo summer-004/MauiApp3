acd0f26fa972ad8f1377684da0446e107fc83d87
summer-004 <60648402+summer-004@users.noreply.github.com>
2023-04-17 17:15:55 +0800

    Create IDataStore.cs

---
+++ Services/IDataStore.cs
@@ -0,0 +1,11 @@
+namespace MauiApp3.Services
+{
+    public interface IDataStore<T>
+    {
+        Task<bool> AddItemAsync(T item);
+        Task<bool> UpdateItemAsync(T item);
+        Task<bool> DeleteItemAsync(string id);
+        Task<T> GetItemAsync(string id);
+        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }
+}
