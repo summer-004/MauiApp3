1c5524d4419f96fae0b80a8a23ad1d8c20229695
summer-004 <60648402+summer-004@users.noreply.github.com>
2023-04-17 17:12:01 +0800

    Create ItemDetailViewModel.cs

---
+++ ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,62 @@
+using MauiApp3.Views;
+using System.Diagnostics;
+
+namespace MauiApp3.ViewMoels
+{
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        private string itemId;
+        private string text;
+        private string description;
+        public string Id { get; set; }
+        public Command UpdateCommand { get; }
+        public ItemDetailViewModel()
+        {
+            UpdateCommand = new Command(onUpdate);
+        }
+        private async void onUpdate()
+        {
+            await Shell.Current.GoToAsync($"{nameof(UpdateItemPage)}?{nameof(UpdateItemViewModel.ItemId)}={ItemId}");
+        }
+        public string Text
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+
+        public string ItemId
+        {
+            get
+            {
+                return itemId;
+            }
+            set
+            {
+                itemId = value;
+                LoadItemId(value);
+            }
+        }
+        
+        public async void LoadItemId(string itemId)
+        {
+            try
+            {
+                var item = await DataStore.GetItemAsync(itemId);
+                Id = item.Id;
+                Text = item.Text;
+                Description = item.Description;
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Failed to Load Item");
+            }
+        }
+    }
+}
