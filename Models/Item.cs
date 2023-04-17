9aac43c5f0336c8eaf13fff320a9d0c77fcde740
summer-004 <60648402+summer-004@users.noreply.github.com>
2023-04-17 17:14:54 +0800

    Create Item.cs

---
+++ Models/Item.cs
@@ -0,0 +1,9 @@
+namespace MauiApp3.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
